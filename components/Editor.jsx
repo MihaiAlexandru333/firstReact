3c023d938bcaf5b2c9e56d3a380ce8c228c5d2e3
MihaiAlexandru333 <durduvsmihai@yahoo.com>
2023-06-14 12:08:00 +0300

    add debouncing and misc features

--- components/Editor.jsx
+++ components/Editor.jsx
@@ -1,7 +1,8 @@
 import React from "react"
 import Showdown from "showdown"
+import ReactMde from "react-mde"
 
-export default function Editor({ currentNote, updateNote }) {
+export default function Editor({ tempNoteText, setTempNoteText }) {
     const [selectedTab, setSelectedTab] = React.useState("write")
 
     const converter = new Showdown.Converter({
@@ -14,8 +15,8 @@ export default function Editor({ currentNote, updateNote }) {
     return (
         <section className="pane editor">
             <ReactMde
-                value={currentNote.body}
-                onChange={updateNote}
+                value={tempNoteText}
+                onChange={setTempNoteText}
                 selectedTab={selectedTab}
                 onTabChange={setSelectedTab}
                 generateMarkdownPreview={(markdown) =>
